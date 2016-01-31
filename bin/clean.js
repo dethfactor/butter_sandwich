3dd93c1e03385fbfa6b7a28e5388799e22c380f5
Sal Ortiz <sortiz@enova.com>
2016-01-31 17:01:54 -0600

    clean out pkgs and bin links

--- bin/clean.js
+++ bin/clean.js
@@ -3,8 +3,11 @@
 var Script = require( __dirname + '/../lib/script/script.js' );
 
 Script.rmdirRecursive( Script.tmpBasePath );
+Script.rmdirRecursive( Script.pkgPath );
 //TODO: delete dl'd packages and stuff in /res/pkg
 
+Script.rm( Script.binPath + '/nw' );
+Script.rm( Script.binPath + '/nwjc' );
 
 Script.unsetConfiguredTag();
 Script.unsetCurrentArchTag();
